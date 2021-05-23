18bbdbea0de1eaaf5791fcf39b6097c978b7f422
bsottek <bsottek@gmail.com>
2021-05-23 18:51:10 -0500

    contract search functionality

--- js/init.js
+++ js/init.js
@@ -5,3 +5,24 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+var searchBar = function() {
+  var input = $('#search');
+  var filter = input.val().toUpperCase();
+  console.log(filter);
+  var cardContainer = $('#card-container');
+  var contract = $("#contract-card");
+  var titles = document.getElementsByClassName('card-title');
+
+  for (i=0; i<titles.length; i++){
+    txtValue = titles[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      titles[i].parentElement.parentElement.parentElement.style.visibility = "visible";
+    } else {
+      titles[i].parentElement.parentElement.parentElement.style.visibility = "hidden";
+    }
+  }
+}
+
+
