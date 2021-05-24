34eee846ee8801a6b900a5ea9f745c792ab50fb8
bsottek <bsottek@gmail.com>
2021-05-23 21:40:04 -0500

    fixed card reordering on search

--- js/init.js
+++ js/init.js
@@ -11,16 +11,18 @@ var searchBar = function() {
   var input = $('#search');
   var filter = input.val().toUpperCase();
   console.log(filter);
-  var cardContainer = $('#card-container');
-  var contract = $("#contract-card");
+  var cardContainer = $('#card-container-row');
   var titles = document.getElementsByClassName('card-title');
 
   for (i=0; i<titles.length; i++){
+    var contractCard = titles[i].parentElement.parentElement.parentElement;
     txtValue = titles[i].innerText;
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      titles[i].parentElement.parentElement.parentElement.style.visibility = "visible";
+      contractCard.style.visibility = "visible";
+      cardContainer.prepend(contractCard);
     } else {
-      titles[i].parentElement.parentElement.parentElement.style.visibility = "hidden";
+      contractCard.style.visibility = "hidden";
+      cardContainer.append(contractCard);
     }
   }
 }
