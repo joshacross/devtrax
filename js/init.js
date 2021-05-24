0572a890211c3b3b583eff94a5fbafe1b820c891
bsottek <bsottek@gmail.com>
2021-05-23 22:29:46 -0500

    removed contract elements, added calendar element, added date generation for current month

--- js/init.js
+++ js/init.js
@@ -5,3 +5,16 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+var calendar = $('#calendar');
+var daysInMonth = function()  {
+  var now = new Date();
+  return new Date(now.getFullYear(), now.getMonth()+1, 0).getDate();
+}
+
+for (var day = 1; day < 32; day++){
+  var dayEl = document.createElement('div');
+  dayEl.className = 'day';
+  dayEl.innerHTML = day;
+  calendar.append(dayEl);
+}
