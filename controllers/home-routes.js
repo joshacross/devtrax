30693db9f1e9b16bee7b686ad215e0b08d547672
Killa J <joshuaalexandercross@gmail.com>
2021-05-27 00:15:26 -0500

    updated stylesheet routes in main

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -20,53 +20,58 @@ router.get('/', (req, res) => {
   res.render('login');
 });
 
-
-router.get('/', (req, res) => {
-  console.log(req.session);
-
-  Project.findAll({
-    attributes: [
-      'project_id',
-      'project_url',
-      'project_title',
-      'project_description',
-      'services_rendered',
-      'services_rendered_description',
-      'project_start_date',
-      'project_completion_date',
-      'total_price_of_project',
-      'fee_schedule',
-      'length_of_project',
-      'client_first_name',
-      'client_last_name',
-      'client_email_address',
-      'client_company_name',
-      'client_billing_address',
-      'client_city',
-      'client_zipcode',
-      'contract_signed',
-      'contract_created_date',
-      'contract_signed_date',
-      'created_at',
-      'updated_at'
-    ],
-    include: [
-      {
-        model: User,
-        attributes: ['user_id', 'username', 'first_name', 'last_name']
-      }
-    ]
-  })
-    .then(dbProjectData => {
-        const projects = dbProjectData.map(project => project.get({ plain: true}));
-      // pass a single post object into the homepage template
-      res.render('login', { projectdata });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+//Contracts Page 
+router.get('/contracts', (req, res) => {
+  res.render('contracts');
+  return;
 });
+
+// router.get('/', (req, res) => {
+//   console.log(req.session);
+
+//   Project.findAll({
+//     attributes: [
+//       'project_id',
+//       'project_url',
+//       'project_title',
+//       'project_description',
+//       'services_rendered',
+//       'services_rendered_description',
+//       'project_start_date',
+//       'project_completion_date',
+//       'total_price_of_project',
+//       'fee_schedule',
+//       'length_of_project',
+//       'client_first_name',
+//       'client_last_name',
+//       'client_email_address',
+//       'client_company_name',
+//       'client_billing_address',
+//       'client_city',
+//       'client_zipcode',
+//       'contract_signed',
+//       'contract_created_date',
+//       'contract_signed_date',
+//       'created_at',
+//       'updated_at'
+//     ],
+//     include: [
+//       {
+//         model: User,
+//         attributes: ['user_id', 'username', 'first_name', 'last_name']
+//       }
+//     ]
+//   })
+//     .then(dbProjectData => {
+//         const projects = dbProjectData.map(project => project.get({ plain: true}));
+//       // pass a single post object into the homepage template
+//       res.render('login', { projectdata });
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
 
 router.get('/profile', (req, res) => {
   console.log(req.session);
