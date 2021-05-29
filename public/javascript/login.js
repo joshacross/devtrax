b220c928977d4251bb574675259e00bf2df232d1
Killa J <joshuaalexandercross@gmail.com>
2021-05-29 01:53:45 -0500

    updated application with Auth0

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,60 +1,67 @@
 
-// Signup Function
-async function signupFormHandler(event) {
-    event.preventDefault();
+// // Signup Function
+// async function signupFormHandler(event) {
+//     event.preventDefault();
 
-    const username = document.querySelector('#username-signup').value.trim();
-    const firstName = document.querySelector('#firstname-signup').value.trim();
-    const lastName = document.querySelector('#lastname-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+//     const username = document.querySelector('#username-signup').value.trim();
+//     const firstName = document.querySelector('#firstname-signup').value.trim();
+//     const lastName = document.querySelector('#lastname-signup').value.trim();
+//     const email = document.querySelector('#email-signup').value.trim();
+//     const password = document.querySelector('#password-signup').value.trim();
 
-    if (username && email && password && firstName && lastName ) {
-      const response = await fetch('/api/users', {
-        method: 'post',
-        body: JSON.stringify({
-          username,
-          first_name,
-          last_name,
-          email,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
+//     if (username && email && password && firstName && lastName ) {
+//       const response = await fetch('/api/users', {
+//         method: 'post',
+//         body: JSON.stringify({
+//           username,
+//           first_name,
+//           last_name,
+//           email,
+//           password
+//         }),
+//         headers: { 'Content-Type': 'application/json' }
+//       });
 
-      // check the response status
-      if (response.ok) {
-          console.log('success');
-      } else {
-          alert(response.statusText);
-      }
-    }
-  }
+//       // check the response status
+//       if (response.ok) {
+//           console.log('success');
+//       } else {
+//           alert(response.statusText);
+//       }
+//     }
+//   }
 
-  // Login Function
-async function loginFormHandler(event) {
-  event.preventDefault();
+//   // Login Function
+// async function loginFormHandler(event) {
+//   event.preventDefault();
 
-  const email = document.querySelector('#email-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
-  if (email && password) {
-    const response = fetch('/api/users/login', {
-      method: 'post',
-      body: JSON.stringify({
-        email,
-        password
-      }),
-      headers: { 'Content-Type': 'application/json' }
-    });
+//   const email = document.querySelector('#email-login').value.trim();
+//   const password = document.querySelector('#password-login').value.trim();
+//   if (email && password) {
+//     const response = fetch('/api/users/login', {
+//       method: 'post',
+//       body: JSON.stringify({
+//         email,
+//         password
+//       }),
+//       headers: { 'Content-Type': 'application/json' }
+//     });
 
-    // check the response status
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert(response.statusText);
-    }
-  }
-}
+//     // check the response status
+//     if (response.ok) {
+//       document.location.replace('/');
+//     } else {
+//       alert(response.statusText);
+//     }
+//   }
+// }
 
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
-document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+// document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+// document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+
+var webAuth = new auth0.WebAuth({
+  domain:       'jcross.us.auth0.com',
+  clientID:     'yI9F7Df4apEn8wszR3FvfSVivapN9MPd',
+  redirectUri:  '/profile/' + req.session.user_id,
+  
+});
--- server.js
+++ server.js
@@ -1,18 +1,26 @@
-const path = require('path');
 const express = require('express');
+const path = require('path');
 const exphbs = require('express-handlebars');
-
-const app = express();
-const PORT = process.env.PORT || 3001;
 
 const sequelize = require("./config/connection");
 
 // Connect session to Sequelize Database
 const session = require('express-session');
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
+
+require('dotenv').config();
+
+const authRouter = require('./controllers/api/auth');
+
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const sess = {
-  secret: 'Super secret secret',
+const app = express();
+// const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || "8000";
+
+const session = {
+  secret: process.env.SESSION_SECRET,
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -21,12 +29,18 @@ const sess = {
   })
 };
 
-app.use(session(sess));
+if (app.get('env') === "production") {
+  // serve secure cookies, requires HTTPS
+  session.cookie.secure = true;
+};
+
 // End Session Script^
 
 const helpers = require('./utils/helpers');
 
 const hbs = exphbs.create({ helpers });
+
+app.set('views', path.join(__dirname, 'views'));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -34,6 +48,51 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(expressSession(session));
+
+//Passport Config
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Serialize and DeSerialize User Instances to and from session
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+// Creating custom middleware with Express
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated();
+  next();
+});
+
+// Router mounting
+app.use('/', authRouter);
+
+ const strategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL: process.env.AUTH0_CALLBACK_URL
+  },
+  function(accessToken, refreshToken, extraParams, profile, done) {
+    /**
+     * Access tokens are used to authorize users to an API
+     * (resource server)
+     * accessToken is the token to call the Auth0 API
+     * or a secured third-party API
+     * extraParams.id_token has the JSON Web Token
+     * profile has all the information from the user
+     */
+    return done(null, profile);
+  }
+);
 
 app.use(require('./controllers/'));
 
