87d3f05571986b5ade0c439abb90258d6d2eb822
Killa J <joshuaalexandercross@gmail.com>
2021-05-31 00:21:37 -0500

    updated connection.js file

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-// // create connection to our database, pass in your MySQL information for username and password
+// create connection to our database, pass in your MySQL information for username and password
 // const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
 //   host: 'localhost',
 //   dialect: 'mysql',
