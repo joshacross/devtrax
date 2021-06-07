6e9eb0cff9aa17db8600e16d88faa835f2dd612a
Killa J <joshuaalexandercross@gmail.com>
2021-06-06 20:33:16 -0500

    user.js updates

--- public/javascript/user.js
+++ public/javascript/user.js
@@ -1,60 +1,42 @@
 
-// // Signup Function
-// async function signupFormHandler(event) {
-//     event.preventDefault();
+// Signup Function
+async function signupFormHandler(event) {
+    event.preventDefault();
 
-//     const username = document.querySelector('#username-signup').value.trim();
-//     const firstName = document.querySelector('#firstname-signup').value.trim();
-//     const lastName = document.querySelector('#lastname-signup').value.trim();
-//     const email = document.querySelector('#email-signup').value.trim();
-//     const password = document.querySelector('#password-signup').value.trim();
+    const user_first_name = document.querySelector('#user_first_name').value.trim();
+    const user_last_name = document.querySelector('#user_last_name').value.trim();
+    const user_company_name = document.querySelector('#user_company_name').value.trim();
+    const user_billing_address = document.querySelector('#user_billing_address').value.trim();
+    const user_city = document.querySelector('#user_city').value.trim();
+    const user_zipcode = document.querySelector('#user_zipcode').value.trim();
+    const user_id = document.querySelector('#user_id').value.trim();
+    const username = document.querySelector('#username').value.trim();
+    const user_email = document.querySelector('#user_email').value.trim();
 
-//     if (username && email && password && firstName && lastName ) {
-//       const response = await fetch('/api/users', {
-//         method: 'post',
-//         body: JSON.stringify({
-//           username,
-//           first_name,
-//           last_name,
-//           email,
-//           password
-//         }),
-//         headers: { 'Content-Type': 'application/json' }
-//       });
+    if (username && email && password && firstName && lastName ) {
+      const response = await fetch('/api/users', {
+        method: 'post',
+        body: JSON.stringify({
+          user_first_name,
+          user_last_name,
+          user_company_name,
+          user_billing_address,
+          user_city,
+          user_zipcode,
+          user_id,
+          username,
+          user_email
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
 
-//       // check the response status
-//       if (response.ok) {
-//           console.log('success');
-//       } else {
-//           alert(response.statusText);
-//       }
-//     }
-//   }
+      // check the response status
+      if (response.ok) {
+          console.log('success');
+      } else {
+          alert(response.statusText);
+      }
+    }
+  }
 
-//   // Login Function
-// async function loginFormHandler(event) {
-//   event.preventDefault();
-
-//   const email = document.querySelector('#email-login').value.trim();
-//   const password = document.querySelector('#password-login').value.trim();
-//   if (email && password) {
-//     const response = fetch('/api/users/login', {
-//       method: 'post',
-//       body: JSON.stringify({
-//         email,
-//         password
-//       }),
-//       headers: { 'Content-Type': 'application/json' }
-//     });
-
-//     // check the response status
-//     if (response.ok) {
-//       document.location.replace('/');
-//     } else {
-//       alert(response.statusText);
-//     }
-//   }
-// }
-
-// document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
-// document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+document.querySelector('.user-form').addEventListener('submit', loginFormHandler);
