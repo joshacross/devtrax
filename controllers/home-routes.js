973c21c0a1614703bc305e6b190acb8342fd4806
Killa J <joshuaalexandercross@gmail.com>
2021-06-06 23:08:17 -0500

    debugging routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const { Project, User } = require('../models');
+// const db = require('../models');
+// const secured = require('../lib/middleware/secured');
 const passport = require('passport');
-require('dotenv').config();
+
 
 router.get(
   "/",
@@ -11,9 +12,7 @@ router.get(
   }),
   (req, res) => {
     res.render('profile');
-  }
-);
-
+  });
 
 // Home Page , if logged in go to user's profile, if not redirect to login
 router.get('/', (req, res) => {
@@ -29,262 +28,130 @@ router.get('/', (req, res) => {
 
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
-  // if (req.session.loggedIn) {
-  //   let nickname = req.session.passport.user.nickname;
-  //   let displayname = req.session.passport.user.displayName;
-  //   let lastname = "";
-  //   let  = req.session.passport.user.displayName;
-  //   if (!nickName) {
-  //     const response = await fetch('/api/users', {
-  //       method: 'post',
-  //       body: JSON.stringify({
-  //         nickname,
-  //         displayname,
-  //         lastname,
-  //         email,
-  //         password
-  //       }),
-  //       headers: { 'Content-Type': 'application/json' }
-  //     });
-
-  //     // check the response status
-  //     if (response.ok) {
-  //         console.log('success');
-  //     } else {
-  //         alert(response.statusText);
-  //     }
-  //   }
-  // }
-// }
-    // if(user !exists){
-    //   // post new user;
-    // }
     res.redirect('/welcome');
     return;
-
   } else {
   res.redirect('/login');
   }
 });
 
-// // Login Post Route, by 
-// router.post('/login', (req, res) => {
-//   User.findOne({
-//     where: {
-//       email: req.session.email
-//     }
-//   }).then(dbUserData => {
-//     if (!dbUserData) {
-//       res.status(400).json({ message: 'No user with that email address!' });
-//       return;
-//     }
-
-//     const validPassword = dbUserData.checkPassword(req.body.password);
-
-//     if (!validPassword) {
-//       res.status(400).json({ message: 'Incorrect password!' });
-//       return;
-//     }
-
-//     req.session.save(() => {
-//       // declare session variables
-//       req.session.user_id = dbUserData.id;
-//       req.session.username = dbUserData.username;
-//       req.session.loggedIn = true;
-
-//       res.json({ user: dbUserData, message: 'You are now logged in!' });
-//     });
-//   });
-// });
-
-// router.get('/logout', (req, res) => {
-//   res.render('login');
-// })
 
 //Welcome Page - Render if user does not exist. If they do exist render profile page.
-router.get('/welcome', async (req, res) => {
+router.get('/welcome', (req, res, next) => {
+  const { _raw, _json, ...userProfile } = req.user;
 
-  let users = await User.findOne({
+  User.findOne({
     where: {
-      auth_id: req.session.passport.user.user_id
+      auth_id: userProfile.id
     }
   })
   .then((dbUserData) => {
     if (!dbUserData) {
-      const userDataId = req.session.passport.user.id;
-      const userName = req.session.passport.user.displayName;
-      const userEmail = req.session.passport.user._json.email;
+      const userDataId = userProfile.id;
+      const userName = userProfile.displayName;
+      const userEmail = _json.email;
   
       res.render('welcome', { userDataId, userName, userEmail });
       return;
     } else {
-    res.render('profile', { users });
+    res.redirect('/profile');
     return;
     }
   });
 });
 
-//Contracts Page 
+router.get('/profile', (req, res, next) => {
+  const { _raw, _json, ...userProfile } = req.user;
+
+  User.findOne({
+    where: {
+      auth_id: userProfile.id
+    },
+    attributes: [
+      'id',
+      'user_first_name',
+      'user_last_name',
+      'username',
+      'user_email'
+    ]
+  })
+  .then((dbUserData => {
+    Project.findAll({
+      attributes: 
+      [
+        'project_id',
+        'project_title',
+        'project_description',
+        'services_rendered',
+        'services_rendered_description',
+        'project_start_date',
+        'project_completion_date',
+        'total_price_of_project',
+        'fee_schedule',
+        'length_of_project',
+        'client_first_name',
+        'client_last_name',
+        'client_email_address',
+        'client_company_name',
+        'client_billing_address',
+        'client_city',
+        'client_zipcode',
+        'contract_signed',
+        'contract_created_date',
+        'contract_signed_date',
+        'user_id',
+        'created_at',
+        'updated_at'
+      ]
+    })
+  }))
+    .then((dbProjectData => {
+      if (!dbProjectData) {
+        User.findOne({
+          where: {
+            auth_id: userProfile.id
+          },
+          attributes: [
+            'id',
+            'user_first_name',
+            'user_last_name',
+            'username',
+            'user_email'
+          ]
+        })
+        .then((dbUserData => {
+        res.render('profile', { dbUserData });
+        return;
+        }))
+      } else {
+      const projects = dbProjectData.map(project => project.get({ plain: true }));
+      // const user = dbUserData.map(user => user.get({ plain: true }));
+      // pass a single post object into the homepage template
+      res.render('profile', { projects });
+      return;
+      }
+      }
+    ))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+
+// Contracts Page 
 // Add info to page - at the top showing user's information that will be added to the contract.
 
 router.get('/contracts', (req, res) => {
-  const userDataId = req.session.passport.user.id;
-  const userName = req.session.passport.user.displayName;
+  const { _raw, _json, ...userProfile } = req.user;
+
+  const userDataId = userProfile.id;
+  const userName = userProfile.displayName;
   const userEmail = req.session.passport.user._json.email;
   
   res.render('contracts', { userDataId, userName, userEmail });
   return;
 });
-
-// //Signup Page
-// router.get('/signup', (req, res) => {
-//   res.render('signup');
-//   return;
-// });
-
-// router.get('/profile', (req, res) => {
-//   console.log(req.session);
-
-//   Project.findAll({
-//     attributes: [
-//       'project_id',
-//       'project_url',
-//       'project_title',
-//       'project_description',
-//       'services_rendered',
-//       'services_rendered_description',
-//       'project_start_date',
-//       'project_completion_date',
-//       'total_price_of_project',
-//       'fee_schedule',
-//       'length_of_project',
-//       'client_first_name',
-//       'client_last_name',
-//       'client_email_address',
-//       'client_company_name',
-//       'client_billing_address',
-//       'client_city',
-//       'client_zipcode',
-//       'contract_signed',
-//       'contract_created_date',
-//       'contract_signed_date',
-//       'user_id',
-//       'username',
-//       'user_email',
-//       'user_first_name',
-//       'user_last_name',
-//       'created_at',
-//       'updated_at'
-//     ]
-//     // include: [
-//     //   {
-//     //     model: User,
-//     //     attributes: ['user_id', 'username', 'first_name', 'last_name']
-//     //   }
-//     // ]
-//   })
-//     .then(dbProjectData => {
-//         const projects = dbProjectData.map(project => project.get({ plain: true}));
-//       // pass a single post object into the homepage template
-//       res.render('profile', { projects });
-//     })
-//     .catch(err => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
-// });
-
-
-// GET /api/projects by user_id
-// router.get('/profile', async (req, res) => {
-//   let projects = await Project.findAll({
-//     where: {
-//       user_id: req.session.passport.user.user_id
-//     },
-//   })
-//   res.render("profile", { projects });
-//   return;
-// });
-
-router.get('/profile',  (req, res) => {
-  let data = req.body;
-  console.log(data);
-  res.render('profile');
-});
-  // const { userProfile } = req.user;
-  // const user = req.session.passport.user;
-  // const userDataId = req.session.passport.user.id;
-  // const userDataId = userProfile.id;
-  // // const userName = req.session.passport.user.displayName;
-  // // const userEmail = req.session.passport.user.emails.value;
-  // // Ping Database by session id to find projects.
-  // // If project doesn't exist, render profile page
-  // // If project exists, render dbProjectData 
-  // console.log(userDataId);
-  // console.log(user);
-//   Project.findAll({
-//     where: {
-//       user_id: req.body.
-//     },
-//     attributes: [
-//       'project_id',
-//       'project_title',
-//       'project_description',
-//       'services_rendered',
-//       'services_rendered_description',
-//       'project_start_date',
-//       'project_completion_date',
-//       'total_price_of_project',
-//       'fee_schedule',
-//       'length_of_project',
-//       'client_first_name',
-//       'client_last_name',
-//       'client_email_address',
-//       'client_company_name',
-//       'client_billing_address',
-//       'client_city',
-//       'client_zipcode',
-//       'contract_signed',
-//       'contract_created_date',
-//       'contract_signed_date',
-//       'user_id',
-//       'created_at',
-//       'updated_at'
-//     ],
-//     include: {
-//       model: User,
-//       attributes: 
-//         [
-//           'id',
-//           'username', 
-//           'user_first_name', 
-//           'user_last_name',
-//           'user_email',
-//           'auth_id', 
-//           'company_name', 
-//           'user_billing_address', 
-//           'user_city',
-//           'user_zipcode'
-//         ]
-//       }
-//   })
-//     .then(dbProjectData => {
-//       if (!dbProjectData) {
-//         res.render('profile');
-//         return;
-//       } else {
-//       const projects = dbProjectData.map(project => project.get({ plain: true }));
-//       const user = dbUserData.map(user => user.get({ plain: true }));
-//       // pass a single post object into the homepage template
-//       res.render('profile', { projects, user });
-//       return;
-//       }
-//     })
-//     .catch(err => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
-// });
 
 // get one user
 router.get('/client-contract-signature/:id', (req, res) => {
@@ -316,23 +183,19 @@ router.get('/client-contract-signature/:id', (req, res) => {
       'contract_created_date',
       'contract_signed_date',
       'user_id',
-      'username',
-      'user_email',
-      'user_first_name',
-      'user_last_name',
       'created_at',
       'updated_at'
     ],
     // order projects based on the most recent project created_at date
-    order: [['created_at', 'DESC']]
+    order: [['created_at', 'DESC']],
     // JOIN to the user table using include
-    // include: [
-    //   //include client model
-    //   {
-    //     model: User,
-    //     attributes: ['user_id', 'username', 'first_name', 'last_name']
-    //   }
-    // ]
+    include: [
+      //include client model
+      {
+        model: User,
+        attributes: ['user_id', 'username', 'user_first_name', 'user_last_name']
+      }
+    ]
   })
     .then(dbProjectData => {
       if (!dbProjectData) {
@@ -377,23 +240,19 @@ router.get('/client-contract/:id', (req, res) => {
       'contract_created_date',
       'contract_signed_date',
       'user_id',
-      'username',
-      'user_email',
-      'user_first_name',
-      'user_last_name',
       'created_at',
       'updated_at'
     ],
     // order projects based on the most recent project created_at date
-    order: [['created_at', 'DESC']]
+    order: [['created_at', 'DESC']],
     // JOIN to the user table using include
-    // include: [
-    //   //include client model
-    //   {
-    //     model: User,
-    //     attributes: ['user_id', 'username', 'first_name', 'last_name']
-    //   }
-    // ]
+    include: [
+      //include client model
+      {
+        model: User,
+        attributes: ['user_id', 'username', 'user_first_name', 'user_last_name']
+      }
+    ]
   })
     .then(dbProjectData => {
       if (!dbProjectData) {
